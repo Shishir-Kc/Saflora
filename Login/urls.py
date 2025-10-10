b8495f49949f6b4346f79d174fa9c3114ea314da
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-10 09:34:20 +0545

    updated login routes

--- Login/urls.py
+++ Login/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import user_login
+from .views import user_login,user_signup
 
 
 app_name = "login"
 
 urlpatterns = [
-    path('',user_login,name="user_login")
+    path('',user_login,name="user_login"),
+    path('user/signup/',user_signup,name='user_signup'),
 ]
 
 
