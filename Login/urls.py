423c569a7c3c4954d780d35c1970514ef90045a2
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-11 19:54:30 +0545

    added path for user logout

--- Login/urls.py
+++ Login/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_login,user_signup
+from .views import user_login,user_signup,user_logout
 
 
 app_name = "login"
@@ -7,6 +7,7 @@ app_name = "login"
 urlpatterns = [
     path('',user_login,name="user_login"),
     path('user/signup/',user_signup,name='user_signup'),
+    path('user/logout/',user_logout,name="user_logout"),
 ]
 
 
