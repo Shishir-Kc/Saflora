6c5ef5fa527a5ea5d19b37cac694e3e3b2b6ae09
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-14 13:14:20 +0545

    added url for otp validation

--- Login/urls.py
+++ Login/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import user_login,user_signup,user_logout
+from .views import (user_login
+                    ,user_signup,
+                    user_logout,
+                    forgot_pass)
 
 
 app_name = "login"
@@ -8,6 +11,8 @@ urlpatterns = [
     path('',user_login,name="user_login"),
     path('user/signup/',user_signup,name='user_signup'),
     path('user/logout/',user_logout,name="user_logout"),
+    path('forgot/password/',forgot_pass,name="forgot_pass"),
+
 ]
 
 
