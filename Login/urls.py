e57093590816bba2cc338ecad77ea18b4fbc92ee
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-15 11:36:25 +0545

    added url for resend otp !

--- Login/urls.py
+++ Login/urls.py
@@ -4,7 +4,7 @@ from .views import (user_login
                     user_logout,
                     forgot_pass,
                     verify_otp,
-                    reset_pass,)
+                    reset_pass,rsend_otp)
 
 
 app_name = "login"
@@ -16,6 +16,7 @@ urlpatterns = [
     path('forgot/password/',forgot_pass,name="forgot_pass"),
     path('forgot/password/otp/verify/',verify_otp,name="otp_verify"),
     path('forgot/password/reset/',reset_pass,name="reset_pass"),
+    path('forgot/password/otp/resend/',rsend_otp,name="resend_otp"),
 ]
 
 
