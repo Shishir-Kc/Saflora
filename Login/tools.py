3af8293fd637403d4ce6f394ab9ad742e7c0b2fa
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-18 18:26:38 +0545

    added tools.py for celery ! to perform background tasks !

--- Login/tools.py
+++ Login/tools.py
@@ -1,15 +1,4 @@
-from django.core.mail import send_mail
 from Accounts.models import Saflora_user
-
-def send_verification_email(email, code):
-    subject = 'Your Verification Code'
-    message = f'Your verification code is: {code}'
-    from_email = 'test@gmail.com'
-    recipient_list = email
-    send_mail(subject, message, from_email, recipient_list)
-    print("Email sent to ", email)
-    return True
-    
 
 def does_user_exists(email=''):
     try:
