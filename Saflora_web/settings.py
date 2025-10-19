bb7afe36f4d762294dda2a47547e4242153f8a95
Rishab <rishabthapa7777@gmail.com>
2025-10-19 21:04:42 +0545

    required icons added

--- Saflora_web/settings.py
+++ Saflora_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-uv7#lw4krmu$$#u!+@r6nn#bh2y^(2!_myy9f99wbbe#7szssk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
