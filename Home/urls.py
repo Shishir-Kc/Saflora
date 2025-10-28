1cabd0e54e949438543d6b390976b75495bee66d
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-28 11:35:46 +0545

    added proprt url routing

--- Home/urls.py
+++ Home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import in_home,base_navbar
+from .views import in_home,base_navbar,products_list,check_out
 
 app_name = "home"
 
@@ -7,5 +7,6 @@ app_name = "home"
 urlpatterns = [
     path('',in_home,name="in_home"),
     path('navbar/',base_navbar,name="base_navbar"),
-
+    path('products/view/',products_list,name="products_list"),
+    path('checkout/<str:item_name>/<uuid:id>/payment/',check_out,name="check_out"),
 ]
