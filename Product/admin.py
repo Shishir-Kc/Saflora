b3458262ea5fb6a852d7064c6f56f928303701d4
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-28 11:38:02 +0545

    upadted products url models and views

--- Product/admin.py
+++ Product/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Saflora_Product
+from .models import Saflora_Product,Feature,Variants
 
 admin.site.register(Saflora_Product)
+admin.site.register(Feature)
+admin.site.register(Variants)
--- Product/models.py
+++ Product/models.py
@@ -3,25 +3,29 @@ import uuid
 
 class Feature(models.Model):
     name  = models.CharField(max_length=200)
-    description = models.TextField()
+
     def __str__(self):
         return self.name
 
-
-
+class Variants(models.Model):
+    variant = models.CharField()
+    class Meta:
+        verbose_name = "variant"
+    def __str__(self):
+        return self.variant
 
 class Saflora_Product(models.Model):
-    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=True)
+    id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     name = models.CharField(max_length=200)
     price = models.FloatField()
     discount_price = models.FloatField(null=True, blank=True)
-    feature = models.ManyToManyField(Feature, blank=True)
+    features = models.ManyToManyField(Feature, blank=True)
     description = models.TextField()
     image = models.ImageField(upload_to='product_images/')
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-
+    variant = models.ManyToManyField(Variants)
     class Meta():
         verbose_name = "Product"
         verbose_name_plural = "Products"
