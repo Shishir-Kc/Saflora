8e50638fe2a19cb006b0a306498d04dcf0d19f95
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-30 18:11:45 +0545

    updated url wth new pages . view

--- Home/urls.py
+++ Home/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import in_home,base_navbar,products_list,check_out
+from .views import (in_home,
+                    base_navbar,
+                    products_list,
+                    check_out,
+                    user_profile,
+                    about_us,
+                    contact,
+                    how_to_use,)
+
 
 app_name = "home"
 
@@ -9,4 +17,9 @@ urlpatterns = [
     path('navbar/',base_navbar,name="base_navbar"),
     path('products/view/',products_list,name="products_list"),
     path('checkout/<str:item_name>/<uuid:id>/payment/',check_out,name="check_out"),
+    path('user/profile/view/',user_profile,name="user_profile"),
+    path('about-us/', about_us, name='about_us'),
+    path('contact/', contact, name='contact'),
+    path('how-to-use/', how_to_use, name='how_to_use'),
+    
 ]
