27f099038bde2ed424870143be3a549596cc8938
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-31 21:06:49 +0545

    feat:updated url routes

--- Home/urls.py
+++ Home/urls.py
@@ -6,7 +6,13 @@ from .views import (in_home,
                     user_profile,
                     about_us,
                     contact,
-                    how_to_use,)
+                    how_to_use,
+                    cart,
+                    add_to_cart,
+                    update_address,
+                    update_profile,
+                    delete_cart,
+                    )
 
 
 app_name = "home"
@@ -16,10 +22,16 @@ urlpatterns = [
     path('',in_home,name="in_home"),
     path('navbar/',base_navbar,name="base_navbar"),
     path('products/view/',products_list,name="products_list"),
-    path('checkout/<str:item_name>/<uuid:id>/payment/',check_out,name="check_out"),
+    path('checkout/<uuid:id>/payment/',check_out,name="check_out"),
+     path('checkout/<uuid:id>/payment/<uuid:cart_id>/',check_out,name="check_out_cart"),
     path('user/profile/view/',user_profile,name="user_profile"),
     path('about-us/', about_us, name='about_us'),
     path('contact/', contact, name='contact'),
     path('how-to-use/', how_to_use, name='how_to_use'),
+    path('user/cart/',cart,name="cart"),
+    path('user/cart/add/<uuid:product_id>/',add_to_cart,name="add_to_cart"),
+    path('user/update/account/',update_profile,name="update_profile"),
+    path('user/update/address/',update_address,name="update_address"),
+    path('user/delete/cart/<uuid:id>/',delete_cart,name="delete_cart")
     
 ]
