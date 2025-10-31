62f5fef5925bcda1ece37657a4ae294d2b4a324c
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-31 21:09:15 +0545

    idk

--- Product/admin.py
+++ Product/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Saflora_Product,Feature,Variants
+from .models import Saflora_Product,Feature,Variants,Saflora_Base_Product
 
 admin.site.register(Saflora_Product)
 admin.site.register(Feature)
 admin.site.register(Variants)
+admin.site.register(Saflora_Base_Product)
