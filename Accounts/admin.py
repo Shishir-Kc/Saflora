97cb96a70bd02a6335003e638afb97777db5ba19
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-11-01 17:36:37 +0545

    aded location / wada

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Saflora_user,Cart
+from .models import Saflora_user,Cart,Location
 
 admin.site.register(Saflora_user)
 admin.site.register(Cart)
+admin.site.register(Location)
