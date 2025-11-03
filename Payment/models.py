7e139d21fedf3519f9474d940ef4f8b349189106
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-11-03 18:41:35 +0545

    feat: updated models data

--- Payment/models.py
+++ Payment/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 import uuid
 from Product.models import Saflora_Product
-from Accounts.models import Saflora_user
+from Accounts.models import Saflora_user,AnonymousUser,Cart
+from datetime import timedelta,datetime
+from django.utils import timezone
+
+def expires_at():
+   return timezone.now() + timedelta(minutes=1)
 
 class Payment_Records(models.Model):
     class Status(models.TextChoices):
@@ -16,12 +21,22 @@ class Payment_Records(models.Model):
     class Payment_Provider(models.TextChoices):
        KHALTI = "KHALTI","Khalti"
        ESEWA = "ESEWA","Esewa"
+    class Payment_Method(models.TextChoices):
+       ONLINE = "ONLINE","Online"
+       COD = "COD",'Cod'
 
-    user = models.ForeignKey(Saflora_user,verbose_name=("User"), on_delete=models.CASCADE)
+
+    user = models.ForeignKey(Saflora_user,verbose_name=("User"), on_delete=models.CASCADE,null=True)
+    anonymous_user = models.ForeignKey(AnonymousUser,null=True,on_delete=models.CASCADE)
     id = models.UUIDField(default=uuid.uuid4,editable=False,primary_key=True)
     product = models.ForeignKey(Saflora_Product, verbose_name="Product",on_delete=models.CASCADE,null=True)
     status =models.CharField(choices=Status.choices,verbose_name='Payment Status  ')  
     created_at = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(choices=Payment_Method.choices,null=True)
+    payment_session = models.DateTimeField(default=expires_at)
+   #cart
+    cart = models.ForeignKey(Cart, verbose_name=("Cart"), on_delete=models.CASCADE,null=True) 
+
 
     # service porviders 
 
@@ -39,7 +54,11 @@ class Payment_Records(models.Model):
 
 
     def __str__(self):
-       return f"{self.user}`s order of {self.product}"
+       if self.user:
+          user = self.user
+       elif self.anonymous_user:
+         user = self.anonymous_user.email
+       return f"{user}`s order of {self.product}"
 
 
 
