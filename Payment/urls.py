d578e36df3d5095994b8fb0910c6ab01c608d014
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-11-03 18:42:35 +0545

    marked esewa for now

--- Payment/urls.py
+++ Payment/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import payment,khalti_payment,validate_khalti_payment,esewa_payment
+from .views import khalti_payment,validate_khalti_payment
 
 app_name = "payment"
 
 urlpatterns = [
-    path('esewa/',esewa_payment,name="esewa_payment"),
+    # path('esewa/',esewa_payment,name="esewa_payment"),
     path('khalti/<uuid:id>/cart/<uuid:cart_id>/',khalti_payment,name="Khalti_payment"),   
     path('khalti/verification/<uuid:cart_id>/',validate_khalti_payment,name='validate_khalti_payment'), 
 ]
