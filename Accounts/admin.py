09bc5db90ad6c474ac4025507ae2f59f73973871
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-11-07 20:04:55 +0545

    feat: added provience in admin page

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Saflora_user,Cart,Location,AnonymousUser
+from .models import Saflora_user,Cart,Location,AnonymousUser,Province
 
 admin.site.register(Saflora_user)
 admin.site.register(Cart)
 admin.site.register(Location)
 admin.site.register(AnonymousUser)
+admin.site.register(Province)
