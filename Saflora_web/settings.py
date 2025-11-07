3ff81ba9cab8d959918c6c9099d071b8e61384e4
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-11-07 20:25:22 +0545

    rem:removed middleware

--- Saflora_web/settings.py
+++ Saflora_web/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'Login.middlewares.is_Loggedin'
+    'Login.middlewares.is_Loggedin',
 ]
 
 ROOT_URLCONF = 'Saflora_web.urls'
